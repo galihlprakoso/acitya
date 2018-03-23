0da249dd2c43d96217e839612417fce6af6e2dfe
krislase <krislase@gmail.com>
2018-03-23 20:54:59 +0700

    coba

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f00',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
